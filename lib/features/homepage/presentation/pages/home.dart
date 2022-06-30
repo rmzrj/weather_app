132a1926423a8fbf39087aebff14e48d107d05e3
ramees <rameesraj534@gmail.com>
2022-06-30 23:02:18 +0530

    added: weather model and icon hex data

--- lib/features/homepage/presentation/pages/home.dart
+++ lib/features/homepage/presentation/pages/home.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-
-      ),
+      body: Container(),
     );
   }
 }
